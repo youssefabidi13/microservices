3d4fe125512a4e95b636c702fcd9145765fa7626
unknown <youssefabidi929@gmail.com>
2024-12-11 19:09:00 +0100

    Implementing cross cutting concerns tracings loggins using gateway part 1

--- section9/gatewayserver/src/main/java/com/abidiyoussef/gatewayserver/GatewayserverApplication.java
+++ section9/gatewayserver/src/main/java/com/abidiyoussef/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,11 @@ package com.abidiyoussef.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -9,5 +14,28 @@ public class GatewayserverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
+
+	@Bean
+	public RouteLocator abidiBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+						.route(p -> p
+								.path("/abidibank/accounts/**")
+								.filters( f -> f.rewritePath("/abidibank/accounts/(?<segment>.*)","/${segment}")
+										.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.uri("lb://ACCOUNTS"))
+					.route(p -> p
+							.path("/eazybank/loans/**")
+							.filters( f -> f.rewritePath("/abidibank/loans/(?<segment>.*)","/${segment}")
+									.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+							.uri("lb://LOANS"))
+					.route(p -> p
+							.path("/eazybank/cards/**")
+							.filters( f -> f.rewritePath("/abidibank/cards/(?<segment>.*)","/${segment}")
+									.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+							.uri("lb://CARDS")).build();
+
+
+	}
+
 
 }
