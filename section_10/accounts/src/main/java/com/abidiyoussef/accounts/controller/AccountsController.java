45f7edf4be02e3a2272babe49243a9fda476c997
unknown <youssefabidi929@gmail.com>
2024-12-14 15:34:14 +0100

    implement retry pattern in accounts

--- section_10/accounts/src/main/java/com/abidiyoussef/accounts/controller/AccountsController.java
+++ section_10/accounts/src/main/java/com/abidiyoussef/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.abidiyoussef.accounts.dto.CustomerDto;
 import com.abidiyoussef.accounts.dto.ErrorResponseDto;
 import com.abidiyoussef.accounts.dto.ResponseDto;
 import com.abidiyoussef.accounts.service.IAccountsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -15,6 +16,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -32,6 +35,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/api", produces ={MediaType.APPLICATION_JSON_VALUE})
 @Validated
 public class AccountsController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AccountsController.class);
 
     private IAccountsService iAccountsService;
     @Autowired
@@ -189,11 +194,20 @@ public class AccountsController {
             )
     }
     )
+    @Retry(name = "getBuildInfo",fallbackMethod = "getBuildInfoFallback")
     @GetMapping("/build-info")
     public ResponseEntity<String> getBuildInfo() {
+        logger.debug("getBuildInfo() method Invoked");
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(buildVersion);
+    }
+
+    public ResponseEntity<String> getBuildInfoFallback(Throwable throwable) {
+        logger.debug("getBuildInfoFallback() method Invoked");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("0.9");
     }
     @Operation(
             summary = "Get Java version",
