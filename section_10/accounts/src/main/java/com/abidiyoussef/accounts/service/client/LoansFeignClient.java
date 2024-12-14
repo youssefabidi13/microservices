dd5cf2cc61f31c2c25f7fe41975c349b8cf67cff
unknown <youssefabidi929@gmail.com>
2024-12-14 13:13:19 +0100

    implementing circuit breaker pattern in gateway

---
+++ section_10/accounts/src/main/java/com/abidiyoussef/accounts/service/client/LoansFeignClient.java
@@ -0,0 +1,17 @@
+package com.abidiyoussef.accounts.service.client;
+
+import com.abidiyoussef.accounts.dto.CardsDto;
+import com.abidiyoussef.accounts.dto.LoansDto;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "loans")
+public interface LoansFeignClient {
+    @GetMapping(value = "/api/fetch",consumes = "application/json")
+    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestHeader("abidiBank-correlation-id")
+                                                         String correlationId,@RequestParam String mobileNumber);
+}
