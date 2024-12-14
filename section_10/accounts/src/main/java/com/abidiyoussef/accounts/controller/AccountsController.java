a79528b6459acc026d3d9e4569725b2e6618212d
unknown <youssefabidi929@gmail.com>
2024-12-14 16:36:41 +0100

    implementing rate limit pattern in account and gateway

--- section_10/accounts/src/main/java/com/abidiyoussef/accounts/controller/AccountsController.java
+++ section_10/accounts/src/main/java/com/abidiyoussef/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.abidiyoussef.accounts.dto.CustomerDto;
 import com.abidiyoussef.accounts.dto.ErrorResponseDto;
 import com.abidiyoussef.accounts.dto.ResponseDto;
 import com.abidiyoussef.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -227,11 +228,18 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name= "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @Operation(
