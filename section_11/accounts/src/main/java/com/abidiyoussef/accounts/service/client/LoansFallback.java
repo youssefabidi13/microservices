60722efac8fc5fa76680370f38e6f24f0c5ad8c6
unknown <youssefabidi929@gmail.com>
2024-12-15 18:40:11 +0100

    implementing logging using graphana loki alloy

---
+++ section_11/accounts/src/main/java/com/abidiyoussef/accounts/service/client/LoansFallback.java
@@ -0,0 +1,14 @@
+package com.abidiyoussef.accounts.service.client;
+
+import com.abidiyoussef.accounts.dto.LoansDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoansFallback implements LoansFeignClient{
+
+    @Override
+    public ResponseEntity<LoansDto> fetchLoanDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
