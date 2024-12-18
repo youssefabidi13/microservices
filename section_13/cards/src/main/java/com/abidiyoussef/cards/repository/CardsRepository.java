4a19fc0be7c3c738514ae5333a83de49d5e7121b
unknown <youssefabidi929@gmail.com>
2024-12-18 15:38:53 +0100

    develop message microservice using spring cloud functions

---
+++ section_13/cards/src/main/java/com/abidiyoussef/cards/repository/CardsRepository.java
@@ -0,0 +1,16 @@
+package com.abidiyoussef.cards.repository;
+
+import com.abidiyoussef.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+}
